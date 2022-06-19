0e4d193ae358464aa4a6e03a20f882ebf2bb7d45
Ondrej Kotuláč <ondrejkotulac@gmail.com>
2022-06-19 18:04:36 +0200

    Revert "Update CMakeLists.txt"
    This reverts commit 33ae19dbbfd8ed7f42f7b5459c55f89b9997e77b.

---
+++ .build2/CMakeFiles/apfsutil.dir/DependInfo.cmake
@@ -0,0 +1,61 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Administrator/Documents/GitHub/paragon_apfs_sdk_ce/crypt/cipher/aescipher.cpp" "CMakeFiles/apfsutil.dir/crypt/cipher/aescipher.cpp.obj" "gcc" "CMakeFiles/apfsutil.dir/crypt/cipher/aescipher.cpp.obj.d"
+  "C:/Users/Administrator/Documents/GitHub/paragon_apfs_sdk_ce/crypt/cipher/cipherfactory.cpp" "CMakeFiles/apfsutil.dir/crypt/cipher/cipherfactory.cpp.obj" "gcc" "CMakeFiles/apfsutil.dir/crypt/cipher/cipherfactory.cpp.obj.d"
+  "C:/Users/Administrator/Documents/GitHub/paragon_apfs_sdk_ce/crypt/hash/hash.cpp" "CMakeFiles/apfsutil.dir/crypt/hash/hash.cpp.obj" "gcc" "CMakeFiles/apfsutil.dir/crypt/hash/hash.cpp.obj.d"
+  "C:/Users/Administrator/Documents/GitHub/paragon_apfs_sdk_ce/linutil/apfsutil.cpp" "CMakeFiles/apfsutil.dir/linutil/apfsutil.cpp.obj" "gcc" "CMakeFiles/apfsutil.dir/linutil/apfsutil.cpp.obj.d"
+  "C:/Users/Administrator/Documents/GitHub/paragon_apfs_sdk_ce/linutil/debug.cpp" "CMakeFiles/apfsutil.dir/linutil/debug.cpp.obj" "gcc" "CMakeFiles/apfsutil.dir/linutil/debug.cpp.obj.d"
+  "C:/Users/Administrator/Documents/GitHub/paragon_apfs_sdk_ce/linutil/misc.cpp" "CMakeFiles/apfsutil.dir/linutil/misc.cpp.obj" "gcc" "CMakeFiles/apfsutil.dir/linutil/misc.cpp.obj.d"
+  "C:/Users/Administrator/Documents/GitHub/paragon_apfs_sdk_ce/linutil/ufsdio.cpp" "CMakeFiles/apfsutil.dir/linutil/ufsdio.cpp.obj" "gcc" "CMakeFiles/apfsutil.dir/linutil/ufsdio.cpp.obj.d"
+  "C:/Users/Administrator/Documents/GitHub/paragon_apfs_sdk_ce/linutil/ufsdlog.cpp" "CMakeFiles/apfsutil.dir/linutil/ufsdlog.cpp.obj" "gcc" "CMakeFiles/apfsutil.dir/linutil/ufsdlog.cpp.obj.d"
+  "C:/Users/Administrator/Documents/GitHub/paragon_apfs_sdk_ce/linutil/ufsdmmgr.cpp" "CMakeFiles/apfsutil.dir/linutil/ufsdmmgr.cpp.obj" "gcc" "CMakeFiles/apfsutil.dir/linutil/ufsdmmgr.cpp.obj.d"
+  "C:/Users/Administrator/Documents/GitHub/paragon_apfs_sdk_ce/linutil/ufsdstr.cpp" "CMakeFiles/apfsutil.dir/linutil/ufsdstr.cpp.obj" "gcc" "CMakeFiles/apfsutil.dir/linutil/ufsdstr.cpp.obj.d"
+  "C:/Users/Administrator/Documents/GitHub/paragon_apfs_sdk_ce/linutil/ufsdtime.cpp" "CMakeFiles/apfsutil.dir/linutil/ufsdtime.cpp.obj" "gcc" "CMakeFiles/apfsutil.dir/linutil/ufsdtime.cpp.obj.d"
+  "C:/Users/Administrator/Documents/GitHub/paragon_apfs_sdk_ce/ufs/ufsd/src/apfs/apfs.cpp" "CMakeFiles/apfsutil.dir/ufs/ufsd/src/apfs/apfs.cpp.obj" "gcc" "CMakeFiles/apfsutil.dir/ufs/ufsd/src/apfs/apfs.cpp.obj.d"
+  "C:/Users/Administrator/Documents/GitHub/paragon_apfs_sdk_ce/ufs/ufsd/src/apfs/apfsbplustree.cpp" "CMakeFiles/apfsutil.dir/ufs/ufsd/src/apfs/apfsbplustree.cpp.obj" "gcc" "CMakeFiles/apfsutil.dir/ufs/ufsd/src/apfs/apfsbplustree.cpp.obj.d"
+  "C:/Users/Administrator/Documents/GitHub/paragon_apfs_sdk_ce/ufs/ufsd/src/apfs/apfscompr.cpp" "CMakeFiles/apfsutil.dir/ufs/ufsd/src/apfs/apfscompr.cpp.obj" "gcc" "CMakeFiles/apfsutil.dir/ufs/ufsd/src/apfs/apfscompr.cpp.obj.d"
+  "C:/Users/Administrator/Documents/GitHub/paragon_apfs_sdk_ce/ufs/ufsd/src/apfs/apfsencryption.cpp" "CMakeFiles/apfsutil.dir/ufs/ufsd/src/apfs/apfsencryption.cpp.obj" "gcc" "CMakeFiles/apfsutil.dir/ufs/ufsd/src/apfs/apfsencryption.cpp.obj.d"
+  "C:/Users/Administrator/Documents/GitHub/paragon_apfs_sdk_ce/ufs/ufsd/src/apfs/apfsenum.cpp" "CMakeFiles/apfsutil.dir/ufs/ufsd/src/apfs/apfsenum.cpp.obj" "gcc" "CMakeFiles/apfsutil.dir/ufs/ufsd/src/apfs/apfsenum.cpp.obj.d"
+  "C:/Users/Administrator/Documents/GitHub/paragon_apfs_sdk_ce/ufs/ufsd/src/apfs/apfshash.cpp" "CMakeFiles/apfsutil.dir/ufs/ufsd/src/apfs/apfshash.cpp.obj" "gcc" "CMakeFiles/apfsutil.dir/ufs/ufsd/src/apfs/apfshash.cpp.obj.d"
+  "C:/Users/Administrator/Documents/GitHub/paragon_apfs_sdk_ce/ufs/ufsd/src/apfs/apfsinode.cpp" "CMakeFiles/apfsutil.dir/ufs/ufsd/src/apfs/apfsinode.cpp.obj" "gcc" "CMakeFiles/apfsutil.dir/ufs/ufsd/src/apfs/apfsinode.cpp.obj.d"
+  "C:/Users/Administrator/Documents/GitHub/paragon_apfs_sdk_ce/ufs/ufsd/src/apfs/apfssuper.cpp" "CMakeFiles/apfsutil.dir/ufs/ufsd/src/apfs/apfssuper.cpp.obj" "gcc" "CMakeFiles/apfsutil.dir/ufs/ufsd/src/apfs/apfssuper.cpp.obj.d"
+  "C:/Users/Administrator/Documents/GitHub/paragon_apfs_sdk_ce/ufs/ufsd/src/apfs/apfstable.cpp" "CMakeFiles/apfsutil.dir/ufs/ufsd/src/apfs/apfstable.cpp.obj" "gcc" "CMakeFiles/apfsutil.dir/ufs/ufsd/src/apfs/apfstable.cpp.obj.d"
+  "C:/Users/Administrator/Documents/GitHub/paragon_apfs_sdk_ce/ufs/ufsd/src/apfs/apfsvolsb.cpp" "CMakeFiles/apfsutil.dir/ufs/ufsd/src/apfs/apfsvolsb.cpp.obj" "gcc" "CMakeFiles/apfsutil.dir/ufs/ufsd/src/apfs/apfsvolsb.cpp.obj.d"
+  "C:/Users/Administrator/Documents/GitHub/paragon_apfs_sdk_ce/ufs/ufsd/src/apfs/apfsxattr.cpp" "CMakeFiles/apfsutil.dir/ufs/ufsd/src/apfs/apfsxattr.cpp.obj" "gcc" "CMakeFiles/apfsutil.dir/ufs/ufsd/src/apfs/apfsxattr.cpp.obj.d"
+  "C:/Users/Administrator/Documents/GitHub/paragon_apfs_sdk_ce/ufs/ufsd/src/apfs/dirapfs.cpp" "CMakeFiles/apfsutil.dir/ufs/ufsd/src/apfs/dirapfs.cpp.obj" "gcc" "CMakeFiles/apfsutil.dir/ufs/ufsd/src/apfs/dirapfs.cpp.obj.d"
+  "C:/Users/Administrator/Documents/GitHub/paragon_apfs_sdk_ce/ufs/ufsd/src/common/crc32.cpp" "CMakeFiles/apfsutil.dir/ufs/ufsd/src/common/crc32.cpp.obj" "gcc" "CMakeFiles/apfsutil.dir/ufs/ufsd/src/common/crc32.cpp.obj.d"
+  "C:/Users/Administrator/Documents/GitHub/paragon_apfs_sdk_ce/ufs/ufsd/src/common/fsbase.cpp" "CMakeFiles/apfsutil.dir/ufs/ufsd/src/common/fsbase.cpp.obj" "gcc" "CMakeFiles/apfsutil.dir/ufs/ufsd/src/common/fsbase.cpp.obj.d"
+  "C:/Users/Administrator/Documents/GitHub/paragon_apfs_sdk_ce/ufs/ufsd/src/common/uavl.cpp" "CMakeFiles/apfsutil.dir/ufs/ufsd/src/common/uavl.cpp.obj" "gcc" "CMakeFiles/apfsutil.dir/ufs/ufsd/src/common/uavl.cpp.obj.d"
+  "C:/Users/Administrator/Documents/GitHub/paragon_apfs_sdk_ce/ufs/ufsd/src/lzfse/ulzfse.cpp" "CMakeFiles/apfsutil.dir/ufs/ufsd/src/lzfse/ulzfse.cpp.obj" "gcc" "CMakeFiles/apfsutil.dir/ufs/ufsd/src/lzfse/ulzfse.cpp.obj.d"
+  "C:/Users/Administrator/Documents/GitHub/paragon_apfs_sdk_ce/ufs/ufsd/src/unixfs/unixblock.cpp" "CMakeFiles/apfsutil.dir/ufs/ufsd/src/unixfs/unixblock.cpp.obj" "gcc" "CMakeFiles/apfsutil.dir/ufs/ufsd/src/unixfs/unixblock.cpp.obj.d"
+  "C:/Users/Administrator/Documents/GitHub/paragon_apfs_sdk_ce/ufs/ufsd/src/unixfs/unixdir.cpp" "CMakeFiles/apfsutil.dir/ufs/ufsd/src/unixfs/unixdir.cpp.obj" "gcc" "CMakeFiles/apfsutil.dir/ufs/ufsd/src/unixfs/unixdir.cpp.obj.d"
+  "C:/Users/Administrator/Documents/GitHub/paragon_apfs_sdk_ce/ufs/ufsd/src/unixfs/unixenum.cpp" "CMakeFiles/apfsutil.dir/ufs/ufsd/src/unixfs/unixenum.cpp.obj" "gcc" "CMakeFiles/apfsutil.dir/ufs/ufsd/src/unixfs/unixenum.cpp.obj.d"
+  "C:/Users/Administrator/Documents/GitHub/paragon_apfs_sdk_ce/ufs/ufsd/src/unixfs/unixfile.cpp" "CMakeFiles/apfsutil.dir/ufs/ufsd/src/unixfs/unixfile.cpp.obj" "gcc" "CMakeFiles/apfsutil.dir/ufs/ufsd/src/unixfs/unixfile.cpp.obj.d"
+  "C:/Users/Administrator/Documents/GitHub/paragon_apfs_sdk_ce/ufs/ufsd/src/unixfs/unixfs.cpp" "CMakeFiles/apfsutil.dir/ufs/ufsd/src/unixfs/unixfs.cpp.obj" "gcc" "CMakeFiles/apfsutil.dir/ufs/ufsd/src/unixfs/unixfs.cpp.obj.d"
+  "C:/Users/Administrator/Documents/GitHub/paragon_apfs_sdk_ce/ufs/ufsd/src/unixfs/unixinode.cpp" "CMakeFiles/apfsutil.dir/ufs/ufsd/src/unixfs/unixinode.cpp.obj" "gcc" "CMakeFiles/apfsutil.dir/ufs/ufsd/src/unixfs/unixinode.cpp.obj.d"
+  "C:/Users/Administrator/Documents/GitHub/paragon_apfs_sdk_ce/ufs/ufsd/src/unixfs/unixsuperblock.cpp" "CMakeFiles/apfsutil.dir/ufs/ufsd/src/unixfs/unixsuperblock.cpp.obj" "gcc" "CMakeFiles/apfsutil.dir/ufs/ufsd/src/unixfs/unixsuperblock.cpp.obj.d"
+  "C:/Users/Administrator/Documents/GitHub/paragon_apfs_sdk_ce/ufs/ufsd/src/zlib/adler32.cpp" "CMakeFiles/apfsutil.dir/ufs/ufsd/src/zlib/adler32.cpp.obj" "gcc" "CMakeFiles/apfsutil.dir/ufs/ufsd/src/zlib/adler32.cpp.obj.d"
+  "C:/Users/Administrator/Documents/GitHub/paragon_apfs_sdk_ce/ufs/ufsd/src/zlib/crc32.cpp" "CMakeFiles/apfsutil.dir/ufs/ufsd/src/zlib/crc32.cpp.obj" "gcc" "CMakeFiles/apfsutil.dir/ufs/ufsd/src/zlib/crc32.cpp.obj.d"
+  "C:/Users/Administrator/Documents/GitHub/paragon_apfs_sdk_ce/ufs/ufsd/src/zlib/infback.cpp" "CMakeFiles/apfsutil.dir/ufs/ufsd/src/zlib/infback.cpp.obj" "gcc" "CMakeFiles/apfsutil.dir/ufs/ufsd/src/zlib/infback.cpp.obj.d"
+  "C:/Users/Administrator/Documents/GitHub/paragon_apfs_sdk_ce/ufs/ufsd/src/zlib/inffast.cpp" "CMakeFiles/apfsutil.dir/ufs/ufsd/src/zlib/inffast.cpp.obj" "gcc" "CMakeFiles/apfsutil.dir/ufs/ufsd/src/zlib/inffast.cpp.obj.d"
+  "C:/Users/Administrator/Documents/GitHub/paragon_apfs_sdk_ce/ufs/ufsd/src/zlib/inflate.cpp" "CMakeFiles/apfsutil.dir/ufs/ufsd/src/zlib/inflate.cpp.obj" "gcc" "CMakeFiles/apfsutil.dir/ufs/ufsd/src/zlib/inflate.cpp.obj.d"
+  "C:/Users/Administrator/Documents/GitHub/paragon_apfs_sdk_ce/ufs/ufsd/src/zlib/inftrees.cpp" "CMakeFiles/apfsutil.dir/ufs/ufsd/src/zlib/inftrees.cpp.obj" "gcc" "CMakeFiles/apfsutil.dir/ufs/ufsd/src/zlib/inftrees.cpp.obj.d"
+  "C:/Users/Administrator/Documents/GitHub/paragon_apfs_sdk_ce/ufs/ufsd/src/zlib/uncompr.cpp" "CMakeFiles/apfsutil.dir/ufs/ufsd/src/zlib/uncompr.cpp.obj" "gcc" "CMakeFiles/apfsutil.dir/ufs/ufsd/src/zlib/uncompr.cpp.obj.d"
+  "C:/Users/Administrator/Documents/GitHub/paragon_apfs_sdk_ce/ufs/ufsd/src/zlib/uzlib.cpp" "CMakeFiles/apfsutil.dir/ufs/ufsd/src/zlib/uzlib.cpp.obj" "gcc" "CMakeFiles/apfsutil.dir/ufs/ufsd/src/zlib/uzlib.cpp.obj.d"
+  "C:/Users/Administrator/Documents/GitHub/paragon_apfs_sdk_ce/ufs/ufsd/src/zlib/zutil.cpp" "CMakeFiles/apfsutil.dir/ufs/ufsd/src/zlib/zutil.cpp.obj" "gcc" "CMakeFiles/apfsutil.dir/ufs/ufsd/src/zlib/zutil.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ .build2/CMakeFiles/apfsutil.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for apfsutil.
+# This may be replaced when dependencies are built.
